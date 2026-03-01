3b9a3d2bc5896a775094831bd7f1bf9c43a23ff5
Samuel Thelen <sthelen@lcs.on.ca>
2026-03-01 14:26:36 -0500

    End of the 90 minutes, didn't come to far, need to seriously prepare for the next one, next class.

--- MyBigApp/ContentView.swift
+++ MyBigApp/ContentView.swift
@@ -15,17 +15,28 @@ struct ContentView: View {
                 Rectangle()
                     .containerRelativeFrame(.horizontal, count: 3, span: 1, spacing: 1)
                     .containerRelativeFrame(.vertical, count: 4, span: 1,  spacing: 1)
-                VStack{
+                VStack {
                     Text("Genre")
                         .fontWeight(.semibold)
                     
                     Text("Science Fiction")
-                  
+                        
                     Text("Date Started")
                         .fontWeight(.semibold)
+                    Text("April 3, 2025")
+                    
+                    Text("Date Finished")
+                        .fontWeight(.semibold)
+                    Text("April 9, 2025")
 
                 }
             }
+        }
+        VStack {
+            HStack{
+                Text("Star Rating")
+            }
+            .containerRelativeFrame(.horizontal, count: 4, span: 1, spacing: 0)
         }
         .navigationTitle("1984")
         .toolbar {
