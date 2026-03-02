4d905f971b81ccd517f2c0d36ce1d39d30a94b11
Samuel Thelen <sthelen@lcs.on.ca>
2026-03-02 15:22:55 -0500

    I added all the text, i will now work on the spacing.

--- MyBigApp/SwiftUIView.swift
+++ MyBigApp/SwiftUIView.swift
@@ -15,18 +15,35 @@ struct SwiftUIView: View {
                 Text("Temperature Converter")
                     .font(.largeTitle)
                     .fontWeight(.bold)
-                Text("100 °F = 37.8 °C")
-                    .font(.largeTitle)
-                Button(action: {}) {
-                    Text("Save")
-                        .foregroundStyle(.white)
-                        .background(Color.blue)
-                        .clipShape(RoundedRectangle(cornerRadius: 30))
+                VStack(spacing: 25) {
+                    Text("100 °F = 37.8 °C")
+                        .font(.largeTitle)
+                    Button(action: {}) {
+                        Text("Save")
+                            .foregroundStyle(.white)
+                            .background(Color.blue)
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                            
+                    }
                 }
                 
                 VStack(alignment: .leading, spacing: 30) {
-                    Text("100")
-                    Text("History")
+                    Divider()
+                    Text("    100")
+                    Divider()
+                    Text("  History")
+                        .fontWeight(.bold)
+                        .font(.title3)
+                    Text("    100 °F = 37.8 °C")
+                        .font(.largeTitle)
+                    Divider()
+                    Text("    0 °F = -17 °C")
+                        .font(.largeTitle)
+                    Divider()
+                    Text("    70 °F = 21.1 °C")
+                        .font(.largeTitle)
+                    Divider()
+
                 }
             }
             }
